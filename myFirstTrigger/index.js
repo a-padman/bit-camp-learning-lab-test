18240e384917f38881611a53f3aa49af689ed398
a-padman <apadman22@gmail.com>
2021-01-28 21:16:16 -0600

    Week 2

--- myFirstTrigger/index.js
+++ myFirstTrigger/index.js
@@ -1,14 +1,52 @@
+var multipart = require("parse-multipart");
+var fetch = require("node-fetch");
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    // const name = (req.query.name || (req.body && req.body.name));
-    // const responseMessage = name
-    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully!!"
-    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-    let now = new Date(); 
-    responseMessage = now;
+    context.log('JavaScript HTTP trigger function processed a request.'); 
+
+    var boundary = multipart.getBoundary(req.headers['content-type']);
+    
+    var body = req.body;
+  
+    var parts = multipart.Parse(body, boundary);
+
+    var result = await analyzeImage(parts[0].data);
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: {
+            result
+        }
     };
+
+    console.log(result)
+    context.done(); 
+};
+
+async function analyzeImage(img){
+    const subscriptionKey = process.env['face_key'];
+    const endpoint = process.env['face_endpoint'];
+
+    const uriBase = `${endpoint}/face/v1.0/detect`;
+
+
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'emotion'
+    })
+
+    
+    //COMPLETE THE CODE
+    let resp = await fetch(uriBase + '?' + params.toString(), {
+        method: 'POST',  //WHAT TYPE OF REQUEST?
+        body: img,  //WHAT ARE WE SENDING TO THE API?
+        headers: {
+            'Content-Type': 'application/octet-stream',  //do this in the next section
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        }
+    })
+
+    let data = await resp.json();
+    
+    return data; 
 }
+
